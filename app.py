7353064f4839d142d9740cfdfa04f39cda48dca6
SiggiS-HD <100769097+SiggiS-HD@users.noreply.github.com>
2025-04-29 15:39:38 +0200

    app.py an gunicorn Aufruf anpassen

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def index():
     return render_template('index.html', files=files, selected_file=selected_file, html_content=html_content)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
